71b9dda1c2afc5aacf380bec3a8035a1530a4a2b
MehmetGezen <mehmetgznarch96@gmail.com>
2022-11-17 04:25:13 +0100

    react app crreatetd

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <nav class="navbar bg-light">
+        <form class="container-fluid">
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1"><img src="" alt="" /></span>
+            <input type="text" class="form-control" placeholder="Search" aria-label="Search"/>
+          </div>
+        </form>
+      </nav>
+    </>
+  );
+}
+
+export default App;
